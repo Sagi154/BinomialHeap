3c4f8626b3b86c057da5d46bad37420cb4f1243d
Sagi154 <sagihe@mail.tau.ac.il>
2024-03-11 11:27:22 +0200

    Added test for meld

--- src/BinomialHeap/testing.java
+++ src/BinomialHeap/testing.java
@@ -56,6 +56,26 @@ public class testing {
         return heap;
     }
 
+    public static BinomialHeap insertRandomOrderRandomKeys (int size, int range) {
+        Random random = new Random();
+        BinomialHeap heap = new BinomialHeap();
+        int key;
+        Integer[] keysInserted = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            keysInserted[i] = random.nextInt(1, range + 1);
+        }
+
+        List<Integer> list = Arrays.asList(keysInserted);
+        Collections.shuffle(list);
+        list.toArray(keysInserted);
+
+        for (int i = 0; i < size; i++) {
+            key = keysInserted[i];
+            heap.insert(key, "aa");
+        }
+        return heap;
+    }
+
     public static BinomialHeap testInsert(){
         System.out.println("---------Testing insert-----------");
         BinomialHeap heap = new BinomialHeap();
@@ -73,33 +93,33 @@ public class testing {
         heap.insert(2464, "aa");
 
         PrintHeap.printHeap(heap, true);
-        heap.decreaseKey(item839, 193);
+//        heap.decreaseKey(item839, 193);
         PrintHeap.printHeap(heap, true);
         heap.delete(item839);
         PrintHeap.printHeap(heap, true);
 
 
-        BinomialHeap heaps = new BinomialHeap();
-        System.out.println("000000000000000000000000000000000 heaps 00000000000000000000000000000000");
-        heaps.insert(647, "aa");
-        heaps.insert(2405, "aa");
-        heaps.insert(1871, "aa");
-        heaps.insert(2464, "aa");
-        PrintHeap.printHeap(heaps, true);
-
-        //HeapGraph.draw(heaps);
-
-        System.out.println("00000000000111111111111111111111 heapa 0000000000000111111111111111111111111");
-        BinomialHeap heapa = new BinomialHeap();
-        heapa.insert(1190, "aa");
-        heapa.insert(1647, "aa");
-        heapa.insert(3647, "aa");
-        heapa.insert(3830, "aa");
-        heapa.insert(1743, "aa");
-        heapa.insert(2424, "aa");
-        heapa.insert(4209, "aa");
-        PrintHeap.printHeap(heapa, true);
-        HeapGraph.draw(heapa);
+//        BinomialHeap heaps = new BinomialHeap();
+//        System.out.println("000000000000000000000000000000000 heaps 00000000000000000000000000000000");
+//        heaps.insert(647, "aa");
+//        heaps.insert(2405, "aa");
+//        heaps.insert(1871, "aa");
+//        heaps.insert(2464, "aa");
+//        PrintHeap.printHeap(heaps, true);
+//
+//        //HeapGraph.draw(heaps);
+//
+//        System.out.println("00000000000111111111111111111111 heapa 0000000000000111111111111111111111111");
+//        BinomialHeap heapa = new BinomialHeap();
+//        heapa.insert(1190, "aa");
+//        heapa.insert(1647, "aa");
+//        heapa.insert(3647, "aa");
+//        heapa.insert(3830, "aa");
+//        heapa.insert(1743, "aa");
+//        heapa.insert(2424, "aa");
+//        heapa.insert(4209, "aa");
+//        PrintHeap.printHeap(heapa, true);
+//        HeapGraph.draw(heapa);
 
 
 
@@ -107,13 +127,35 @@ public class testing {
 
         return heap;
     }
-    public static void main (String[] args){
-//        testInsert(9);
-//        BinomialHeap heap = insertRandomOrder1ToCount(20);
-        BinomialHeap heap = testInsert();
-//        PrintHeap.printHeap(heap, true);
+
+
+    public static void testCreateHeapFromNodeChildren (){
+        // First we create a binomial tree
+        BinomialHeap heap = insertRandomOrder1ToCount(1);
+        System.out.println("The heap we're testing looks like: ");
+        PrintHeap.printHeap(heap, true);
+        System.out.println("Heap last is: \n" + heap.getLast().getItem());
+        System.out.println("Heap min is: \n" + heap.getMin().getItem());
+        BinomialHeap heapnew = BinomialHeap.createHeapFromNodeChildren(heap.getLast());
+        System.out.println("The heap we're getting looks like: ");
+        PrintHeap.printHeap(heapnew, true);
+//        HeapGraph.draw(heapnew);
+    }
+
+    public static void testMeld(){
+        int size2 = 9;
+        int size1 = 7;
+        BinomialHeap heap = insertRandomOrderRandomKeys(size1, size1 * 10);
+        PrintHeap.printHeap(heap, true);
+        BinomialHeap heap2 = insertRandomOrderRandomKeys(size2, size2 * 10);
+        PrintHeap.printHeap(heap2, true);
+        heap.meld(heap2);
         HeapGraph.draw(heap);
-//        PrintHeap.printHeap(heap, true);
+
+
+    }
+    public static void main (String[] args){
+        testMeld();
     }
 
 }
